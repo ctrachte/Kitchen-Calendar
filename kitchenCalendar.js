1ce627262a8c54d3f933b2efc6fded3bbab271af
Caleb Trachte <cetrachte@hotmail.com>
2023-10-02 09:00:26 -0500

    add base files, template files

---
+++ kitchenCalendar.js
@@ -0,0 +1,13 @@
+class KitchenCalendar {
+  constructor(options) {
+    this.container = options.containerElement || document.createElement("div");
+  }
+  // THIS METHOD REQUIRED TO MAKE YOUR APP FUNCTION
+  async initialize() {}
+  drawCalendar() {}
+  reset() {}
+  refresh() {}
+  async getData() {}
+  openEvent() {}
+  validate(App) {}
+}
